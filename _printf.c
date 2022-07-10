20748fc1b921c2a4d6b43cc2d24b3a5999cf1e68
mykie88 <mykhelmyers@gmail.com>
2022-07-10 15:00:48 -0700

    wrote printf func to handle char, strings and va_list length

---
+++ _printf.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - function entry point
+ * @format: string specifier
+ * @...: variable argument list
+ *
+ * Return: int
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int len = 0;
+	va_list arg_list;
+	const char *ptr, *str;
+	/*conv_type func[] = {
+		{'%': print_conv_spfr},
+	}*/
+
+	va_start(arg_list, format);
+	for (ptr = format; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == 'c')
+			{
+				_putchar(va_arg(arg_list, int));
+				len++;
+			}
+			else if (*ptr == 's')
+			{
+				str = va_arg(arg_list, char *);
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					len++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(*ptr);
+			len++;
+		}
+	}
+	va_end(arg_list);
+
+	return (len);
+}
+
+/* Main function just for preliminary testing purposes*/
+int main(void)
+{
+	int x, y;
+	char a = 'M';
+	char b[] = "Michael";
+
+	x = _printf("Hello, %c, %s\n", a, b);
+	y = printf("Hello, %c, %s\n",a, b);
+	printf("%d, %d\n", x, y);
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,7 +1,19 @@
+#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+/**
+ * struct - a struct that defines the conversion specifier
+ * @c: format specifier
+ * @conv_func: pointer to conversion function
+ */
+typedef struct conv_sp
+{
+	char c;
+	void (*conv_func)(va_list);
+} conv_type;
 
 #endif
