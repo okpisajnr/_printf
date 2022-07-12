67187870debecb24ebd9d52bc6d8d82a28f94d0b
okpisa felix michael <okpisamichael@gmail.com>
2022-07-12 03:30:14 -0700

    hot fix

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-
+	
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
@@ -38,7 +38,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-       	_printf("Unknown:[%r]\n");
+	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
 }
